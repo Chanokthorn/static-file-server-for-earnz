e4ff8f9aa0c81fba63103b6f0976b660a3b0b0df
Chanokthorn Uerpairojkit <chanokthorn.uer@bkp.co.th>
2020-05-02 19:49:12 +0700

    third commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var express = require("express");
-var serveStatic = require("serve-static");
+const serveStatic = require("serve-static");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
-var app = express();
-
-console.log("serving static file at port 3000");
-app.use(serveStatic("public", { index: ["index.html", "index.htm"] }));
-app.use(express.static("public"));
-app.listen(3000);
+express()
+  .use(express.static(path.join(__dirname, "public")))
+  //   .set("views", path.join(__dirname, "views"))
+  //   .get("/", (req, res) => res.render("pages/index"))
+  //   .use(serveStatic("public", { index: ["index.html", "index.htm"] }))
+  .get("/", function (req, res) {
+    res.sendFile(__dirname + "/pages/index.html");
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
