529f49f012856e214cbc27945170665134c5ac19
Chanokthorn Uerpairojkit <chanokthorn.uer@bkp.co.th>
2020-05-02 19:30:45 +0700

    second commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var serveStatic = require("serve-static");
+
+var app = express();
+
+console.log("serving static file at port 3002");
+app.use(serveStatic("public", { index: ["index.html", "index.htm"] }));
+app.use(express.static("public"));
+app.listen(3002);
