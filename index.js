2bced8c65b71f28b7db9ff025e3206f7503e4545
Chanokthorn Uerpairojkit <chanokthorn.uer@bkp.co.th>
2020-05-06 13:27:36 +0700

    added git addeed new page content

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, "public")))
-  //   .set("views", path.join(__dirname, "views"))
-  //   .get("/", (req, res) => res.render("pages/index"))
-  //   .use(serveStatic("public", { index: ["index.html", "index.htm"] }))
   .get("/", function (req, res) {
     res.sendFile(__dirname + "/pages/index.html");
+  })
+  .get("/random_pics", function (req, res) {
+    res.sendFile(__dirname + "/pages/randomPics.html");
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
